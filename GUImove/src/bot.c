108505897a88aac063e96b60275480aac585f154
Dr-Zelos <72142469+Dr-Zelos@users.noreply.github.com>
2021-04-21 15:56:09 +0200

    Update bot.c

--- GUImove/src/bot.c
+++ GUImove/src/bot.c
@@ -12,4 +12,20 @@ void bot_move(struct bot boat, struct Stack path){
     player_dir.x = cosf(boat.dir);
     player_dir.y = - sinf(boat.dir);
     
+    int angle_dir; //give the direction of the angle: 0 is right, 1 is left
+    float angle_value; //the value of the angle in radian
+    
+    float preangle = (player_dir.x * target_dir.x - player_dir.y * target_dir.y) /
+        sqrtf(player_dir.x * player_dir.x + player_dir.y * player_dir.y) *
+        sqrtf(target_dir.x * target_dir.x + target_dir.y * target_dir.y);
+    
+    if (preangle > 0.0)
+    {angle_dir = 0;
+     angle_value = acos(preangle);}
+    else
+    {angle_dir = 1;
+    angle_value = acos(-(preangle))}
+    
+    
+    
 }
